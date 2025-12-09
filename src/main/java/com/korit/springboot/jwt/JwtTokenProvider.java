a2b9315d9c1dea23d58ffd6539d028c439a8ec6e
smithrowe-10 <smj0418smj@gmail.com>
2025-12-09 10:15:01 +0900

    20251209

--- src/main/java/com/korit/springboot/jwt/JwtTokenProvider.java
+++ src/main/java/com/korit/springboot/jwt/JwtTokenProvider.java
@@ -1,8 +1,39 @@
 package com.korit.springboot.jwt;
 
+import com.korit.springboot.entity.UserEntity;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
 
 @Component
 public class JwtTokenProvider {
+
+    private final SecretKey key;
+
+    public JwtTokenProvider(@Value("${jwt.secret}") String secret) {
+        key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String createAccessToken(UserEntity userEntity) {
+
+        Date now = new Date();
+        Long expiredTime = now.getTime() + (1000L * 60L * 60L * 24L);
+        Date expiredData = new Date(expiredTime);
+
+        return Jwts.builder()
+                .subject("server access token")
+                .issuer("김준일")
+                .issuedAt(new Date())
+                .expiration(expiredData)
+                .claim("userId", userEntity.getUserId())
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
 
 }
