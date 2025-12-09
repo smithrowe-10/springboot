d6146474b5b75f20f87a5c16cf889f6c9779b5df
smithrowe-10 <smj0418smj@gmail.com>
2025-12-09 17:44:28 +0900

    20251209

--- src/main/java/com/korit/springboot/controller/SecurityRequirementController.java
+++ src/main/java/com/korit/springboot/controller/SecurityRequirementController.java
@@ -1,8 +1,11 @@
 package com.korit.springboot.controller;
 
+import com.korit.springboot.security.PrincipalUser;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,9 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
     public class SecurityRequirementController {
 
         @Operation(security = @SecurityRequirement(name = "Bearer Authentication"))
-
         @GetMapping("/api/security")
-        public ResponseEntity<?> get() {
+        public ResponseEntity<?> get(@AuthenticationPrincipal PrincipalUser principalUser) {
+            System.out.println(principalUser.getUserId());
+
+        PrincipalUser principalUser2 = (PrincipalUser) SecurityContextHolder.getContext()
+                .getAuthentication()
+                .getPrincipal();
+        System.out.println(principalUser2.getUserId());
+
+            System.out.println(principalUser2.getUserEntity());
+
             return ResponseEntity.ok("응답");
         }
     }
--- src/main/java/com/korit/springboot/security/PrincipalUser.java
+++ src/main/java/com/korit/springboot/security/PrincipalUser.java
@@ -1,6 +1,7 @@
 package com.korit.springboot.security;
 
 import com.korit.springboot.entity.UserEntity;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -10,6 +11,7 @@ import java.util.Collection;
 
 @RequiredArgsConstructor
 public class PrincipalUser implements UserDetails {
+    @Getter
     private final UserEntity userEntity;
 
     @Override
